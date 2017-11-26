35c39a7f5940464396838a0222d90b82b41292fe
Cristian Nistor <contact@nistorcristian.com>
2017-11-26 11:08:50 +0200

    Fixed load more. Improved listing on responsive.

--- leap/assets/js/main.js
+++ leap/assets/js/main.js
@@ -14,8 +14,7 @@ jQuery(document).ready(function($) {
 
     var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
         h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0),
-        msnry,
-        step = 0;
+        msnry;
 
     // Execute on load
     $(window).load(function() {
@@ -54,86 +53,105 @@ jQuery(document).ready(function($) {
             });
         }
 
-    });
+        var currentPage = 1;
+        var pathname = window.location.pathname;
+        var $result = $('.grid-item');
+        var step = 0;
 
-    // Load more posts on click
-    if (config['load-more']) {
+        // remove hash params from pathname
+        pathname = pathname.replace(/#(.*)$/g, '').replace('/\//g', '/');
 
-        $('#load-posts').addClass('visible');
+        // Load more posts on click
+        if (config['load-more']) {
 
-        var nextPage = 2;
-        var pagination = $('#load-posts').attr('data-posts_per_page');
+            $('#load-posts').addClass('visible');
 
-        $('#load-posts').on('click', function(event) {
-            event.preventDefault();
+            $('#load-posts').on('click', function(event) {
+                event.preventDefault();
 
-            var $this = $(this);
+                var $this = $(this);
 
-            var parseUrl = '&include=tags&limit=' + pagination + '&page=' + nextPage;
-            if ($('body').attr('data-author')) {
-                parseUrl = parseUrl + '&filter=author:' + $('body').attr('data-author');
-            }else if($('body').attr('data-tag')){
-                parseUrl = parseUrl + '&filter=tag:' + $('body').attr('data-tag');
-            }
+                // next page
+                currentPage++;
 
-            if ($this.hasClass('was-dots')) {
-                setTimeout(function() {
-                    console.log($(this));
-                   $this.removeClass('was-dots').addClass('dots');
-                   step = 0;
-                }, 1000);
-            };
+                // Load more
+                var nextPage = pathname + 'page/' + currentPage + '/';
 
-            $.ajax({
-                url: ghost.url.api("posts") + parseUrl,
-                type: 'get'
-            }).done(function(data) {
-                $.each(data.posts, function(i, post) {
-                    $.ajax({
-                        url: ghost.url.api("users") + '&filter=id:' + post.author,
-                        type: 'get'
-                    }).done(function(data) {
-                        $.each(data.users, function(i, users) {
-                            insertPost(post, users);
-                        });
-                    });
-                });
-            }).done(function(data) {
-                var sum = nextPage*pagination;
-                if (sum >= data.meta.pagination.total) {
+                if ($this.hasClass('was-dots')) {
                     setTimeout(function() {
-                        $('#load-posts').addClass('end').text('No more posts');
-                    }, 500);
-                }
-                nextPage += 1;
-            }).fail(function(err) {
-                console.log(err);
-            });
-        });
-    };
-
-    // Infinite scroll
-    if (config['infinite-scroll'] && config['load-more']) {
-        var checkTimer = 'on';
-        if ($('#load-posts').length > 0) {
-            $('#load-posts').addClass('dots');
-            $(window).on('scroll', function(event) {
-                var timer;
-                if (isScrolledIntoView('#load-posts') && checkTimer == 'on' && step < config['infinite-scroll-step']) {
-                    $('#load-posts').addClass('load').click();
-                    checkTimer = 'off';
-                    step++;
-                    timer = setTimeout(function() {
-                        $('#load-posts').removeClass('load');
-                        checkTimer = 'on';
-                        if (step == config['infinite-scroll-step']) {
-                            $('#load-posts').removeClass('dots').addClass('was-dots');
-                        };
+                       $this.removeClass('was-dots').addClass('dots');
+                       step = 0;
                     }, 1000);
                 };
+
+                $.get(nextPage, function (content) {
+                    step++;
+                    var post = $(content).find('.post');
+                    post.find('.content-holder').addClass('no-opacity');
+                    $('#content .grid').append( post );
+                    $.each(post, function(index, val) {
+                        var $this = $(this);
+                        var id = $(this).attr('data-id');
+                        $('#content .grid').imagesLoaded( function() {
+                            expand($this);
+
+                            var a = $this.find('.post-title a');
+                            a.html(a.html().replace(/^(\w+)/, '<span>$1</span>'));
+
+                            msnry.appended( $this );
+                            var animeOpts = {
+                                duration: 800,
+                                easing: [0.1,1,0.3,1],
+                                delay: function(t,i) {
+                                    return i*35;
+                                },
+                                opacity: {
+                                    value: [0,1],
+                                    duration: 600,
+                                    easing: 'linear'
+                                },
+                                translateY: [200,0],
+                                translateZ: [300,0],
+                                rotateX: [75,0]
+                            }
+
+                            animeOpts.targets = '.post[data-id="'+ id +'"] .content-holder';
+                            anime.remove(animeOpts.targets);
+                            anime(animeOpts);
+                        });
+                    });
+                }).always(function () {
+                    if (currentPage == maxPages) {
+                        $('#load-posts').addClass('end').text('No more posts');
+                    };
+                });
+
             });
         };
-    };
+
+        // Infinite scroll
+        if (config['infinite-scroll'] && config['load-more']) {
+            var checkTimer = 'on';
+            if ($('#load-posts').length > 0) {
+                $('#load-posts').addClass('dots');
+                $(window).on('scroll', function(event) {
+                    var timer;
+                    if (isScrolledIntoView('#load-posts') && checkTimer == 'on' && step < config['infinite-scroll-step']) {
+                        $('#load-posts').addClass('load').click();
+                        checkTimer = 'off';
+                        timer = setTimeout(function() {
+                            $('#load-posts').removeClass('load');
+                            checkTimer = 'on';
+                            if (step == config['infinite-scroll-step']) {
+                                $('#load-posts').removeClass('dots').addClass('was-dots');
+                            };
+                        }, 1000);
+                    };
+                });
+            };
+        };
+    });
+
 
     $( document ).ajaxStart(function() {
         if ($('#load-posts').hasClass('dots load')) {
@@ -256,124 +274,6 @@ jQuery(document).ready(function($) {
     // Get first number of words from a string
     function getWords(str) {
         return str.split(/\s+/).slice(0,22).join(" ");
-    }
-
-    // Append posts on masonry container
-    function insertPost(postData, authorData) {
-
-        var d = postData.published_at.slice(0, 10).split('-');
-        var monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-        var monthNumber = d[1];
-        if (monthNumber.slice(0,1) == '0') {
-            monthNumber = monthNumber.slice(1,2) - 1;
-        }else{
-            monthNumber--;
-        };
-
-        var featured = '';
-
-        if (postData.featured) {
-            featured = 'featured';
-        };
-
-        if (d[2].slice(0,1) == '0') {
-            d[2] = d[2].slice(1,2);
-        }
-
-        var datetime = d[0] +'-'+ d[1] +'-'+ d[2];
-        var date = monthNames[monthNumber] + ' ' + d[2] + ', ' + d[0];
-        var excerpt;
-        if (postData.custom_excerpt != null) {
-            excerpt = postData.custom_excerpt;
-        }else{
-            excerpt = getWords($(postData.html).text());
-        };
-
-        var data = {
-            comment_id: postData.comment_id,
-            title: postData.title,
-            date: {
-                "datetime": datetime,
-                "date": date
-            },
-            featured: featured,
-            url: postData.url,
-            excerpt: excerpt,
-            author: {
-                "slug": authorData.slug,
-                "name": authorData.name
-            },
-            tags: function(){
-                if (!$.isEmptyObject(postData.tags)) {
-                    data.tags.tag = postData.tags.slice( 0, 5 );
-                    return true;
-                };
-            },
-            feature_image: function(){
-                if (postData.feature_image != '' && postData.feature_image != null) {
-                    return postData.feature_image;
-                };
-            },
-        }
-
-        var template = [
-            '<article class="post grid-item {{#featured}}featured{{/featured}} {{#tags}}{{#tags.tag}}tag-{{slug}} {{/tags.tag}}{{/tags}}" data-id="{{comment_id}}">',
-                '<div class="content-holder">',
-                    '<div class="post-meta {{#feature_image}}has-image{{/feature_image}}">',
-                        '{{#tags}}',
-                            '<ul class="tags">',
-                            '{{#tags.tag}}',
-                                '<li>',
-                                    '<a href="/tag/{{slug}}" title="{{name}}" class="tag tag-{{id}} {{slug}}">{{name}}</a>',
-                                '</li>',
-                            '{{/tags.tag}}',
-                            '</ul>',
-                        '{{/tags}}',
-                        '<a href="{{url}}" title="{{title}}" class="img-holder">',
-                            '{{#feature_image}}',
-                                '<img src="{{feature_image}}" alt="{{title}}">',
-                            '{{/feature_image}}',
-                        '</a>',
-                        '<time class="post-date" datetime="{{date.datetime}}">{{date.date}}</time>',
-                    '</div>',
-                    '<h2 class="post-title"><a href="{{url}}" title="{{title}}">{{title}}</a></h2>',
-                    '<p>',
-                        '{{excerpt}}',
-                    '</p>',
-                '</div>',
-            '</article>'
-        ].join("\n");
-
-        var post = Mustache.render(template, data);
-        post = $(post);
-        post.find('.content-holder').addClass('no-opacity');
-        $('#content .grid').append( post );
-        $('#content .grid').imagesLoaded( function() {
-            expand(post);
-
-            var a = post.find('.post-title a');
-            a.html(a.html().replace(/^(\w+)/, '<span>$1</span>'));
-
-            msnry.appended( post );
-            var animeOpts = {
-                duration: 800,
-                easing: [0.1,1,0.3,1],
-                delay: function(t,i) {
-                    return i*35;
-                },
-                opacity: {
-                    value: [0,1],
-                    duration: 600,
-                    easing: 'linear'
-                },
-                translateY: [200,0],
-                translateZ: [300,0],
-                rotateX: [75,0]
-            }
-            animeOpts.targets = '.post[data-id="'+ postData.comment_id +'"] .content-holder';
-            anime.remove(animeOpts.targets);
-            anime(animeOpts);
-        });
     }
 
     // Expand post image if is too small
